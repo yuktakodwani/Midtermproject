20622e95f02077ba7ae4d8d319bd1c96ae507aac
vijijay <43656201+vijijay@users.noreply.github.com>
2018-10-09 17:26:37 -0400

    added project

---
+++ Sample_project/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_project
+{
+   class Program
+    {                  
+
+        public static void Main(string[] args)
+        {
+            
+            string[] AuthorName = new string[5];
+            AuthorName[0] = "SidneySheldon";
+            AuthorName[1] = "Jeffrey Archer";
+            AuthorName[2] = "Dan Brown";
+            AuthorName[3] = "R.K.Narayan";
+            AuthorName[4] = "John Grisham";
+
+            for (int i = 0; i<AuthorName.Length; i++)
+            {
+                if (AuthorName[i].Equals("Dan Brown"))
+                {
+                    Console.WriteLine("Author found");
+                }
+               Console.WriteLine(AuthorName[i]);
+
+                Console.ReadLine();
+
+                //Console.WriteLine(AuthorName[1]);
+
+            }
+            
+
+
+
+
+
+
+    }
+
+    }
+}
