d7cbc4b45a6d6941e32467bef35b786008650fb0
yuktakodwani <43656207+yuktakodwani@users.noreply.github.com>
2018-10-11 18:43:23 -0400

    deleted library and book class and created a new book class

---
+++ Sample_project/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_project
+{
+    public  class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public genreEnum Genre { get; set; }
+        public int Pub_Year { get; set; }
+        public Book(string title, string author, genreEnum genre, int pub_year)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Pub_Year = pub_year;
+        }
+        public Book() { }
+
+    }
+    
+}
--- Sample_project/Program.cs
+++ Sample_project/Program.cs
@@ -6,39 +6,78 @@ using System.Threading.Tasks;
 
 namespace Sample_project
 {
-   class Program
-    {                  
+    enum genreEnum { fiction, nonfiction, management, biography }
 
-        public static void Main(string[] args)
-        {
-            
-            string[] AuthorName = new string[5];
-            AuthorName[0] = "SidneySheldon";
-            AuthorName[1] = "Jeffrey Archer";
-            AuthorName[2] = "Dan Brown";
-            AuthorName[3] = "R.K.Narayan";
-            AuthorName[4] = "John Grisham";
-
-            for (int i = 0; i<AuthorName.Length; i++)
-            {
-                if (AuthorName[i].Equals("Dan Brown"))
-                {
-                    Console.WriteLine("Author found");
-                }
-               Console.WriteLine(AuthorName[i]);
-
-                Console.ReadLine();
-
-                //Console.WriteLine(AuthorName[1]);
-            }
-            
-
-
-
-
-
-
+    enum menuOptionEnum : byte
+    {
+        findBook = 1,
+        findBookbyAuthor = 2,
+        findBookbyGenre = 3,
+        quit = 9,
     }
 
+    class Program
+    {
+        private static string menuText =
+        "\n" +
+        "******************************************** \n" +
+        "Welcome to Library.\n" +
+        "Choose the digit as per the action shown below\n" +
+        "Find Book by Title  : 1\n" +
+        "Find Book by Author : 2\n" +
+        "Find Book by Genre  : 3\n" +
+        "Quit                : 9\n" +
+         "******************************************** \n" +
+        "\n" +
+        "Enter your Option (Digit)  :  ";
+
+        private static byte menuOptionFromUser;
+
+        private static void readMenuInput()
+        {
+            switch (menuOptionFromUser)
+            {
+                case (byte)menuOptionEnum.findBook:
+                    Console.WriteLine("This will find book by title");
+                    displayMenu();
+                    break;
+                case (byte)menuOptionEnum.findBookbyAuthor:
+                    Console.WriteLine("This will find book by author");
+                    displayMenu();
+                    break;
+                case (byte)menuOptionEnum.findBookbyGenre:
+                    Console.WriteLine("This will find book by genre");
+                    displayMenu();
+                    break;
+                case (byte)menuOptionEnum.quit:
+                    Console.WriteLine("Good Bye");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Option ....... Please select the right menu option");
+                    displayMenu();
+                    break;
+            }
+        }
+
+        private static void displayMenu()
+        {
+            Console.WriteLine(menuText);
+            menuOptionFromUser = Convert.ToByte(Console.ReadLine());
+            readMenuInput();
+        }
+
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "Malgudi Days";
+            book1.Author = "R. K. Narayan";
+            book1.Genre = genreEnum.fiction;
+
+            Book book2 = new Book("What happended", "Hillory Clinton", genreEnum.biography,2017);
+
+            displayMenu();
+            Console.ReadLine();
+        }
     }
 }
+
